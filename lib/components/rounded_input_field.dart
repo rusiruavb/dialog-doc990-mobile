2cdf7bde742662813e95b2b9980bbeb14057c9c9
Rusiru Abhisheak <rusiru.bandara@redot.global>
2021-08-01 01:20:39 +0530

    Make rounded input field to advanced text form field

--- lib/components/rounded_input_field.dart
+++ lib/components/rounded_input_field.dart
@@ -1,22 +1,94 @@
 import 'package:flutter/material.dart';
 
-class RoundedTextFeild extends StatelessWidget {
+class RoundedTextFeild extends StatefulWidget {
   final ValueChanged<String> onChange;
   final bool isPassword;
+  final String text;
   final bool isNumber;
-  const RoundedTextFeild(
-      {Key key, this.onChange, this.isPassword, this.isNumber})
-      : super(key: key);
+  final bool isPhoneNumber;
+  const RoundedTextFeild({
+    Key key,
+    this.onChange,
+    this.isPassword,
+    this.isNumber,
+    this.text,
+    this.isPhoneNumber = false,
+  }) : super(key: key);
+  @override
+  _RoundedTextFeildState createState() => _RoundedTextFeildState(
+        isNumber: isNumber,
+        isPassword: isPassword,
+        isPhoneNumber: isPhoneNumber,
+        onChange: onChange,
+        text: text,
+      );
+}
 
+class _RoundedTextFeildState extends State<RoundedTextFeild> {
+  final ValueChanged<String> onChange;
+  final bool isPassword;
+  final String text;
+  final bool isNumber;
+  final bool isPhoneNumber;
+  bool _isObscure = true;
+  _RoundedTextFeildState({
+    Key key,
+    this.onChange,
+    this.isPassword,
+    this.isNumber,
+    this.text,
+    this.isPhoneNumber = false,
+  });
   @override
   Widget build(BuildContext context) {
-    return TextFieldContainer(
-      child: TextField(
-        obscureText: isPassword,
-        keyboardType: isNumber ? TextInputType.number : TextInputType.text,
-        style: TextStyle(fontFamily: 'Larsseit', fontSize: 20),
-        onChanged: onChange,
-        decoration: InputDecoration(border: InputBorder.none),
+    final size = MediaQuery.of(context).size;
+    return Container(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            text,
+            style: TextStyle(
+              fontFamily: 'Larsseit',
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+            ),
+            textAlign: TextAlign.left,
+          ),
+          SizedBox(
+            height: size.height * 0.005,
+          ),
+          TextFieldContainer(
+            child: TextField(
+              obscureText: isPassword ? _isObscure : false,
+              maxLength: isPhoneNumber ? 10 : 200,
+              keyboardType:
+                  isNumber ? TextInputType.number : TextInputType.text,
+              style: TextStyle(fontFamily: 'Larsseit', fontSize: 20),
+              onChanged: onChange,
+              decoration: InputDecoration(
+                border: InputBorder.none,
+                counterText: '',
+                suffixIcon: isPassword
+                    ? IconButton(
+                        icon: Icon(
+                          _isObscure ? Icons.visibility : Icons.visibility_off,
+                          color: Colors.red[600],
+                        ),
+                        onPressed: () {
+                          setState(() {
+                            _isObscure = !_isObscure;
+                          });
+                        },
+                      )
+                    : null,
+              ),
+            ),
+          ),
+          SizedBox(
+            height: size.height * 0.025,
+          ),
+        ],
       ),
     );
   }
--- lib/screens/login/login_form.dart
+++ lib/screens/login/login_form.dart
@@ -2,7 +2,19 @@ import 'package:dialog_doc990_mobile/components/rounded_button.dart';
 import 'package:dialog_doc990_mobile/components/rounded_input_field.dart';
 import 'package:flutter/material.dart';
 
-class LoginForm extends StatelessWidget {
+class LoginForm extends StatefulWidget {
+  @override
+  _LoginFormState createState() => _LoginFormState();
+}
+
+class _LoginFormState extends State<LoginForm> {
+  String phoneNumber;
+  String password;
+  _LoginFormState({
+    this.phoneNumber,
+    this.password,
+  });
+
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
@@ -31,40 +43,23 @@ class LoginForm extends StatelessWidget {
             SizedBox(
               height: size.height * 0.025,
             ),
-            Text(
-              'Phone Number',
-              style: TextStyle(
-                fontFamily: 'Larsseit',
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-              ),
-              textAlign: TextAlign.left,
-            ),
-            SizedBox(
-              height: size.height * 0.010,
-            ),
             RoundedTextFeild(
               isPassword: false,
               isNumber: true,
-            ),
-            SizedBox(
-              height: size.height * 0.025,
-            ),
-            Text(
-              'Password',
-              style: TextStyle(
-                fontFamily: 'Larsseit',
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-              ),
-              textAlign: TextAlign.left,
-            ),
-            SizedBox(
-              height: size.height * 0.010,
+              isPhoneNumber: true,
+              text: 'Phone Number',
+              onChange: (text) {
+                phoneNumber = text;
+              },
             ),
             RoundedTextFeild(
               isPassword: true,
               isNumber: false,
+              isPhoneNumber: false,
+              text: 'Password',
+              onChange: (text) {
+                password = text;
+              },
             ),
             SizedBox(
               height: size.height * 0.025,
@@ -74,7 +69,7 @@ class LoginForm extends StatelessWidget {
               child: RoundedButton(
                 text: 'SIGN UP',
                 action: () {
-                  print('Sign Up clicked');
+                  print('Sign Up clicked' + phoneNumber + ' ' + password);
                 },
               ),
             )
